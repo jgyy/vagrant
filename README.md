5338b09a700fcff95700baf9d1b5edc57fd11ad7
jeffrey_goh <jeffrey_goh@tech.gov.sg>
2019-07-06 17:10:30 +0800

    initial commit, not goiong to put gitignore yet

---
+++ README.md
@@ -0,0 +1,2 @@
+## Learn Vagrant Shell Scripting
+Just my personal practice on shell scripting. nothing special here nor any updates to readme.
---
+++ luser-demo01.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This script display various information to the screen.
+
+# Display 'Hello'
+echo 'Hello'
+# Assign a value to a variable
+WORD='script'
+# Display that value using the variable
+echo "$WORD"
+# Demostrate that single quotes cause variables to NOT get expanded.
+echo '$WORD'
+# Combine the variable with hard-coded text.
+echo "This is a shell $WORD"
+# Display the contents of the variable using an alternative syntax.
+echo "This is a shell ${WORD}."
+# Append WORD text to the variable.
+echo "${WORD}ing is fun!"
+# Create a new variable
+ENDING='ed'
+# Combine the two variables.
+echo "This is ${WORD}${ENDING}."
+# Change the value stored in the ENDING variable. (Reassignment.)
+ENDING='ing'
+echo "${WORD}${ENDING} is fun!"
+# Reassign value to ENDING
+ENDING='s'
+echo "You are going to write many ${WORD}${ENDING} in this class!"
