fe86df0c2d5c76629a44197ebb3e045b0920af5b
jeffrey_goh <jeffrey_goh@tech.gov.sg>
2019-07-07 18:04:35 +0800

    works for rhel

--- add-local-user.sh
+++ add-local-user.sh
@@ -2,6 +2,7 @@
 # This script creates a new user on the local system≥
 # You will be prompted to enter the username (login), the person name, and a password.
 # The username, password, and host for the account will be displayed.
+echo 'This script will only work on centOS or Red Hat'
 
 # Make sure the script is being executed with superuser priviledges.
 if [[ "${UID}" -ne 0 ]]
