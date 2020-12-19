6343c529a39164e1a6139fb51368305d57e4cd45
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-12-19 16:17:06 +0800

    completed the second exercise

--- user-account-creation/luser-demo02.sh
+++ user-account-creation/luser-demo02.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+
 # Display the UID and username of the user executing this script.
-# Display if the user is the root user or not.
+# Display of the iser is the root user or not.
 
 # Display the UID
 echo "Your UID is ${UID}"
+
 # Display the username
 USER_NAME=$(id -un)
 echo "Your username is ${USER_NAME}"
+
 # Display if the user is the root user or not.
 if [[ "${UID}" -eq 0 ]]
 then
--- user-account-creation/luser-demo03.sh
+++ user-account-creation/luser-demo03.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-# Display the UID and username of the user executing this script.
-# Display if the user is the ubuntu user or not.
 
-# Display the UID
+# Display the UID and username of the user executing this script.
+# Display if the user is the vagrant user or not.
+
+# Display the UID.
 echo "Your UID is ${UID}"
-# Only display if the UID does NOT match 501.
-UID_TO_TEST_FOR='501'
-if [[ "${UID}" -ne  "${UID_TO_TEST_FOR}" ]]
+
+# Only display if the UID does NOT match 1000.
+UID_TO_TEST_FOR='1000'
+if [[ "${UID}" -ne "${UID_TO_TEST_FOR}" ]]
 then
-    echo "Your UID does not match ${UID_TO_TEST_FOR}"
+    echo "Your UID does not match ${UID_TO_TEST_FOR}."
     exit 1
 fi
+
 # Display the username.
 USER_NAME=$(id -un)
+
 # Test if the command succeeded.
 if [[ "${?}" -ne 0 ]]
 then
@@ -20,16 +24,19 @@ then
     exit 1
 fi
 echo "Your username is ${USER_NAME}"
+
 # You can use a string test conditional.
 USER_NAME_TO_TEST_FOR='jgyy'
-if [[ "${USER_NAME}" == "${USER_NAME_TO_TEST_FOR}" ]]
+if [[ "${USER_NAME}" = "${USER_NAME_TO_TEST_FOR}" ]]
 then
     echo "Your username matches ${USER_NAME_TO_TEST_FOR}."
 fi
+
 # Test for != (not equal) for the string.
 if [[ "${USER_NAME}" != "${USER_NAME_TO_TEST_FOR}" ]]
 then
-    echo "Your username does not match ${USER_NAME_TO_TEST_FOR}"
+    echo "Your username does not match ${USER_NAME_TO_TEST_FOR}."
     exit 1
 fi
+
 exit 0
